50c864aacab8c2196e9a94f3a7c815aef1bed8a8
Ariel Kirkwood <ariel.kirkwood@me.com>
2022-03-16 12:12:48 -0400

    First step

--- test/controllers/dishes_controller_test.rb
+++ test/controllers/dishes_controller_test.rb
@@ -2,12 +2,19 @@ require "test_helper"
 
 class DishesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get new_dish_url
+    menu = menus(:starters)
+    get new_menu_dish_path(menu)
     assert_response :success
   end
 
   test "should post create" do
-    post dishes_url
-    assert_response :success
+    menu = menus(:starters)
+    dish_params = {params: {dish: {name: 'abc', price: 123}}}
+
+    assert_difference("Dish.count") do
+      post menu_dishes_path(menu), dish_params
+    end
+
+    assert_redirected_to menus_path
   end
 end
